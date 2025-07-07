470b7d413538e81f1639054e6a26e8331de57072
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-07-07 15:30:59 +0530

    changed path of image and edit some functionalties

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,15 +3,13 @@ import Table from "./Table";
 
 const Todo = () => {
     const [text, setText] = useState("");
-
     const [tasks, setTasks] = useState([]);
-
     const [storeValue, setStoreValue] = useState("all");
-
     const inputRef = useRef(null);
 
 
-    const addTask = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         if (text.trim() == "") {
             return;
         }
@@ -43,18 +41,18 @@ const Todo = () => {
                 </div>
                 <div className="p-7  scrollbar overflow-y-auto">
                     {tasks.length == 0 ? <div className="flex flex-col items-center justify-center">
-                        <img src="/public/59563746_9318707.jpg" alt="" width={'40%'} />
+                        <img src="/59563746_9318707.jpg" alt="" width={'40%'} />
                         <span className="mt-4 text-xl inline-block">No Task Yet</span>
                     </div> : <Table tasks={tasks} setterTask={setTasks} storeValue={storeValue} />}
                 </div>
 
-                <form className="p-7">
+                <form className="p-7" onSubmit={handleSubmit}>
                     <div className="relative w-6/12 mx-auto">
                         <input ref={inputRef} onChange={(e) => {
                             setText(e.target.value);
                         }} id="default-search" className=" w-full p-4 pr-20  text-m text-gray-900  border-gray-300 rounded-[30px] bg-gray-50   dark:bg-black dark:placeholder-gray-400 dark:text-white" placeholder="Add Task" required />
                         <div className="w-[40px] h-[40px] rounded-[50%] bg-[#333333] absolute end-2.5 bottom-2.5 flex items-center justify-center">
-                            <button type="button" onClick={addTask} className="text-white   font-medium  text-sm px-4 py-2 "><i className="fa-solid fa-arrow-right"></i></button>
+                            <button type="submit" className="text-white   font-medium  text-sm px-4 py-2 "><i className="fa-solid fa-arrow-right"></i></button>
                         </div>
                     </div>
                 </form>
